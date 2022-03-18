3f7ca6fca46ee598c6275c56628578085aa49cf5
FabreVictor <Victor.fabre@lahiette.com>
2022-03-18 08:15:22 +0100

    Update projetA1.py

--- projetA1.py
+++ projetA1.py
@@ -1,8 +1,11 @@
 class Animal:
-    def __init__ (self,name,poids,dodo) :
+    def __init__ (self,name,poids,dodo,cri,couleur,attaque) :
         self.nom = name
         self.poids = poids
         self.dodoJauge = dodo
+        self.cri = cri
+        self.couleur= couleur
+        
     def mange (self,poids,dodoJauge):
         poids += 5
         dodoJauge -= 10
@@ -14,15 +17,23 @@ class Animal:
             print("§§§§§§§§§§§§§§§§§§ le animale é mouru §§§§§§§§§§§§§§§§§§")
         if dodoJauge <= 0:
             print("§§§§§§§§§§§§§§§§§§ le animale é mouru §§§§§§§§§§§§§§§§§§")
+    def mange (self,couleur):
+       self.couleur ="la couleur de l'animel est " + self.couleur
+            
+            
+            
+            
+            
+            
             
 class Chat(Animal):
     
     def __init__ (self,name,couleur):
         self.couleur = couleur 
         
-        Animal.__init__ (self,name,"chat")
-        self.cri = "miaule"
-    def 
+        Animal.__init__ (self,name,)
+
+
         
 monChat = Chat("chien")
 
